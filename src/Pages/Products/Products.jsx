9dd8123243a46b0ed01a5b5d20ac2376f0b1cabf
ramzimammadli <sdfinkod@code.edu.az>
2024-05-10 10:11:29 +0400

    delete thunk

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,34 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addPost, getSliceThunk, postSliceThunk } from "../../Store/Reducers/getSlice";
+import {
+  addPost,
+  deletePostt,
+  deleteSliceThunk,
+  getSliceThunk,
+  postSliceThunk,
+} from "../../Store/Reducers/getSlice";
 
 const Products = () => {
   const [name, setName] = useState("");
   const [des, setDes] = useState("");
-
   const dispatch = useDispatch();
 
   const data = useSelector((state) => state.getSlice.products);
+  // console.log("some", data);
 
   useEffect(() => {
     dispatch(getSliceThunk());
   }, []);
 
-  const dataa = {name,des}
-
   const sendInfo = () => {
-    dispatch(postSliceThunk(dataa))
-    dispatch(addPost(dataa))
-    setDes('')
-    setName('')
+    dispatch(postSliceThunk({ name, des }));
+    dispatch(addPost({ name, des }));
+    setName("");
+    setDes("");
+  };
+
+  const deletePost = (id) => {
+    dispatch(deleteSliceThunk(id))
+    dispatch(deletePostt(id))
   }
+
+  
 
   return (
     <div>
-      <input type="text" name="" id="" value={name} onChange={(e) => setName(e.target.value)}/>
-      <input type="text" name="" id="" value={des} onChange={(e) => setDes(e.target.value)}/>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        name=""
+        id=""
+        value={des}
+        onChange={(e) => setDes(e.target.value)}
+      />
       <button onClick={sendInfo}>Gonder</button>
-      {data && data.map((item) => <p>{item.name}</p>)}
+      {data &&
+        data.map((item) => {
+          return (
+            <div>
+              <p>{item.name}</p>
+              <button onClick={() => deletePost(item.id)}>Sil</button>
+            </div>
+          );
+        })}
     </div>
   );
 };
--- src/store/Reducers/getSlice.js
+++ src/store/Reducers/getSlice.js
@@ -1,49 +1,73 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios from "axios"
 
 export const getSliceThunk = createAsyncThunk('api/get', async () => {
     const res = await axios.get('https://northwind.vercel.app/api/categories')
+    console.log(res.data)
     return res.data
 })
 
 export const postSliceThunk = createAsyncThunk('api/post', async (data) => {
-    const res = await axios.post('https://northwind.vercel.app/api/categories',data)
-    console.log(res.data);
-    return res.data
+    const res = await axios.post('https://northwind.vercel.app/api/categories', data)
+    console.log(res)
+})
+
+export const deleteSliceThunk = createAsyncThunk('api/delete', async (id) => {
+    await axios.delete(`https://northwind.vercel.app/api/categories/${id}`)
 })
 
 const getSlice = createSlice({
     name:'getSlice',
     initialState:{},
     reducers:{
-        addPost: (state, action) => {
+        addPost:(state, action) => {
             state.products.push(action.payload)
+        },
+        deletePostt:(state, action) => {
+            const updatedData = state.products.filter(item => item.id != action.payload)
+            state.products = updatedData
         }
     },
     extraReducers: (builder) => {
         builder
+        //GET
         .addCase(getSliceThunk.fulfilled, (state, action) =>{
             state.loading = false
             state.products = action.payload
         })
-        .addCase(getSliceThunk.pending, (state, action) => {
+        .addCase(getSliceThunk.pending, (state) => {
             state.loading = true
         })
         .addCase(getSliceThunk.rejected, (state, action) => {
             state.loading = false
             state.error = action.error.message
         })
+        //POST
         .addCase(postSliceThunk.fulfilled, (state, action) => {
             state.loading = false
         })
-        .addCase(postSliceThunk.pending, (state, action) => {
+        .addCase(postSliceThunk.pending, (state) => {
             state.loading = true
         })
         .addCase(postSliceThunk.rejected, (state, action) => {
             state.loading = false
             state.error = action.error.message
         })
+        //DELETE
+        .addCase(deleteSliceThunk.fulfilled, (state, action) => {
+            state.loading = false
+        })
+        .addCase(deleteSliceThunk.pending, (state) => {
+            state.loading = true
+        })
+        .addCase(deleteSliceThunk.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.error.message
+        })
+        
     }
 })
-export const {addPost} = getSlice.actions
+
+export const {addPost, deletePostt} = getSlice.actions
+
 export default getSlice.reducer
